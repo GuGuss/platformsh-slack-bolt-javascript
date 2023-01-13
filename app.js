821924d00cc33894d5f40a06f4f13f4e550b2e3f
Augustin Delaporte <augustin@platform.sh>
2023-01-13 22:48:14 +0100

    Prettier

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { App } = require("@slack/bolt");
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
 // This will match any Slack message from channels where the bot has been invited.
@@ -12,40 +12,46 @@ app.message(async ({ message, say }) => {
   var channelId;
   var replyCount;
   var replyMessage;
-  
+
   // Get the channel where the message was posted.
   channelId = message.channel;
-  
+
   // Get the thread that contains the message.
-  if(message.thread_ts) {threadTs = message.thread_ts;} else {threadTs = message.ts;}
-  
+  if (message.thread_ts) {
+    threadTs = message.thread_ts;
+  } else {
+    threadTs = message.ts;
+  }
+
   //console.log (message);
-  
+
   // Get the threads posted to the conversation containing the message.
   try {
     const result = await app.client.conversations.replies({
       token: process.env.SLACK_BOT_TOKEN,
       channel: channelId,
-      ts: threadTs
+      ts: threadTs,
     });
     // Get the number of threads within the conversation.
-    if(result.messages[0]['reply_count']) {replyCount = result.messages[0]['reply_count'];} else {replyCount = 0;}
-  }
-  catch(e){
+    if (result.messages[0]["reply_count"]) {
+      replyCount = result.messages[0]["reply_count"];
+    } else {
+      replyCount = 0;
+    }
+  } catch (e) {
     console.log(e);
   }
-  
-  if(replyCount == process.env.THREAD_NUMBER_LIMIT) { 
-    replyMessage = `Hi :wave:, this thread is becoming quite long (${replyCount} messages)! Have you considered opening an issue or schedule a call to find a solution?`; 
-    // Reply as a thread within the conversation. 
-    await say({text:`${replyMessage}`,thread_ts:threadTs});
+
+  if (replyCount == process.env.THREAD_NUMBER_LIMIT) {
+    replyMessage = `Hi :wave:, this thread is becoming quite long (${replyCount} messages)! Have you considered opening an issue or schedule a call to find a solution?`;
+    // Reply as a thread within the conversation.
+    await say({ text: `${replyMessage}`, thread_ts: threadTs });
   }
-  
 });
 
 (async () => {
-  // Start your app
+  // Start the app.
   await app.start(process.env.PORT || 3000);
 
-  console.log('⚡️ Bolt app is running!');
+  console.log("⚡️ Slack Bolt app is running!");
 })();
